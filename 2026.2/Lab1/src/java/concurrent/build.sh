f5b4d36b75bfd2af5f660c0049374cb7d4f59535
lucasaraujomedeiros <lucasmedeirosjpa@gmail.com>
2026-09-21 17:35:01 -0300

    finish concurrentSolutionV2

--- 2026.2/Lab1/src/java/concurrent/build.sh
+++ 2026.2/Lab1/src/java/concurrent/build.sh
@@ -3,4 +3,4 @@
 BASE_DIR=$(dirname -- "$( readlink -f -- "$0"; )")
 
 # Compila o código direcionando a saída para a pasta bin
-javac -d $BASE_DIR/bin $BASE_DIR/*.java
+javac -d $BASE_DIR/bin $BASE_DIR/SimpleConcurrentSolutionV2.java
